10771219c1698d329dbcabca147ed535c6263b66
Xin Jiang <Jason@Xins-MacBook.local>
2017-05-04 01:59:22 -0500

    Completed Graph and Table

--- GraphTable.java
+++ GraphTable.java
@@ -1,8 +1,37 @@
 
 class GraphTable implements Comparable<GraphTable> {
     
+    private Double distance;
     private Location loc;
-    private 
+
+    public GraphTable(Location loc, double distance) {
+        this.distance = distance;
+        this.loc = loc;
+    }
 
 
+    public Double getDist() {
+        return distance;
+    }
+    
+    public Location getLocation() {
+    	return loc;
+    }
+    
+    public void setDist(double dist) {
+        distance = dist;
+    }
+    
+    
+
+
+    @Override
+    public int compareTo(GraphTable n) {
+       if (distance < n.getDist())
+           return -1;
+       if (distance > n.getDist()) {
+            return 1;
+       }
+        return 0;
+    }
 }
--- NavigationGraph.java
+++ NavigationGraph.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class NavigationGraph implements GraphADT<Location, Path> {
-
+    final private static Double INFINITE = Double.MAX_VALUE;
     private ArrayList<GraphNode<Location, Path>> graph; //adjacency list
     private String[] edgePropertyNames;
 
@@ -138,7 +138,7 @@ public class NavigationGraph implements GraphADT<Location, Path> {
                 List<Path> l = graph.get(i).getOutEdges();
                 //add each dest to the list
                 for(int j = 0; j < l.size(); j++) {
-                   neighbors.add(l.get(i).getDestination());
+                    neighbors.add(l.get(i).getDestination());
                 }
             }
         }
@@ -169,10 +169,60 @@ public class NavigationGraph implements GraphADT<Location, Path> {
         //if cannot find edgePropertyName then throw IllegalArgumentException
         if(edgePropertyNameId == -1)
             throw new IllegalArgumentException();
+        //prepare dist arry
+        Map<Location,Double> distances = new HashMap<Location,Double>();
+        Map<Location,GraphTable> previous = new HashMap<Location,GraphTable>();
 
-       
-        
-        
+        PriorityQueue<GraphTable> nodes = new PriorityQueue();
+
+        for (GraphNode gn: graph) {
+            if (getGraphNode(src) == gn.getId()) {
+                distances.put((Location)gn.getVertexData(), 0.0);
+                nodes.add(new GraphTable((Location)gn.getVertexData(), 0));
+            } else {
+                distances.put((Location)gn.getVertexData(), INFINITE);
+                nodes.add(new GraphTable((Location)gn.getVertexData(),INFINITE));
+            }    
+            previous.put((Location)gn.getVertexData(), null);
+        }
+
+        while (!nodes.isEmpty()) {
+            GraphTable nearest = nodes.poll();
+            if (nearest.getLocation().equals(dest)) {
+                List<Path> paths = new ArrayList();
+                while (previous.get(nearest.getLocation()) != null) {
+                    paths.add(getEdgeIfExists(previous.get(nearest.getLocation()).getLocation(),nearest.getLocation()));
+                    nearest = previous.get(nearest.getLocation());
+                } 
+                return paths;
+
+            } 
+            if (distances.get(nearest.getLocation()).equals(INFINITE))
+                break;
+
+            //get the correspond GraphTable with Location
+            List<Location> neighborsLoc = getNeighbors(nearest.getLocation());
+            List<GraphTable> neighbors = new ArrayList();
+            for (int i = 0; i < nodes.size(); i++) {
+                neighbors.add((GraphTable)nodes.toArray()[i]);
+            }
+            for (GraphTable table : neighbors) {
+                Double newWeight = distances.get(nearest.getLocation()) + table.getDist();
+                if (newWeight < distances.get(table.getLocation()))
+                    distances.put(table.getLocation(), newWeight);
+                previous.put(table.getLocation(), nearest);
+tableRenewloop:
+                for (GraphTable n : nodes) {
+                    if (n.getLocation().equals(table.getLocation())) {
+                        nodes.remove(n);
+                        n.setDist(newWeight);
+                        nodes.add(n);
+                        break tableRenewloop;
+                    }
+                }    
+            }
+        }
+
         return null;
     }
 
